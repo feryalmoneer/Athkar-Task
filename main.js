3500b2d1b0b9fdd706e53e78bf57fa7fd60bfde3
FeryalMoneer <146561102+feryalmoneer@users.noreply.github.com>
2024-02-10 23:17:56 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+var clicks1 = document.getElementById("clicks1").innerHTML;
+function onClick1() {
+    if (clicks1 > 0) {
+        clicks1 -= 1;
+        document.getElementById("clicks1").innerHTML = clicks1;
+    }
+};
+var clicks2 = document.getElementById("clicks2").innerHTML;
+
+function onClick2() {
+    if (clicks2 > 0) {
+        clicks2 -= 1;
+        document.getElementById("clicks2").innerHTML = clicks2;
+    }
+};
+
+var clicks3 = document.getElementById("clicks3").innerHTML;
+
+function onClick3() {
+    if (clicks3 > 0) {
+        clicks3 -= 1;
+        document.getElementById("clicks3").innerHTML = clicks3;
+    }
+};
+
+var clicks4 = document.getElementById("clicks4").innerHTML;
+
+function onClick4() {
+    if (clicks4 > 0) {
+        clicks4 -= 1;
+        document.getElementById("clicks4").innerHTML = clicks4;
+    }
+};
+
+
+
+
+
+
+
+
